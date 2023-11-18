a3d88a3a0f651663f4e42021c6c503f6bcd2d1e2
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-11-18 01:19:48 +0100

    tweede

--- src/components/FelixEerste.vue
+++ src/components/FelixEerste.vue
@@ -2,19 +2,32 @@
 export default{
     data(){
         return{
+            teller : 1,
             header : 'Yes',
             newItem : '',
             dingen:[
                 {id: 1, label:'10 hoedjes'},
                 {id: 2, label:'5 hoedjes'},
                 {id: 3, label:'3 hoedjes'},
-            ]
+            ],
+            uiterobject:{
+                innerobject:{tellertje:0},
+                lijst:['mooi','lelijk']
+            }
         }
     },
     mounted(){
         this.header = 'hoi'
+        ophogen()
     },
     methods:{
+        gelaagd(){
+            this.uiterobject.innerobject.tellertje++
+            this.uiterobject.lijst.push('ertussenin')
+        },
+        ophogen(){
+            this.teller++
+        },
         opslaan(){
             this.header = 'opgeslagen'
             this.dingen.push({id:this.dingen.length + 1, label:this.newItem})
@@ -25,15 +38,19 @@ export default{
 </script>
 <template>
  <div>
-    <h1>{{header}}</h1>
+    <h1>{{header}} ({{teller}})</h1>
     <input 
         @keyup.enter = "dingen.push({id:dingen.length +1, label:newItem})"
         type="text" v-model="newItem" placeholder="Add an Item"
     >
     <button @click="dingen.push({id:dingen.length +1, label:newItem})">save</button>
     <button @click="opslaan">save2viamethods</button>
+    <button @click="ophogen">ophogen</button>
+    <button @click="gelaagd">nested dingen</button>
     <ul>
         <li v-for="ding in dingen" :key="ding.id">{{ding.label}}</li> 
     </ul>
+    <h2>{{uiterobject.innerobject.tellertje}}</h2>
+    <div v-for="soep in uiterobject.lijst"><h1>yes</h1><span>{{soep}}</span></div>
  </div>
 </template>
