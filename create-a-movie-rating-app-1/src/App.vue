a8a320b64517d0895d48f750123d96931fc23b7b
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-12-03 00:12:01 +0100

    posters erbij

---
+++ create-a-movie-rating-app-1/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { items } from "./movies.json";
+import MovieOnderdeel from './components/MovieOnderdeel.vue'
+import { reactive } from 'vue'
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+// import { StarIcon } from "@heroicons/vue/24/solid";
+const films = reactive(items);
+</script>
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-wrap: nowrap; /* Prevents wrapping to the next line */
+  align-items: stretch;
+  justify-content: center; /* Centers items horizontally if they don't fill the entire width */
+}
+</style>
+<template>
+  <!-- This is where your template goes	-->
+  <div class="flex-container">
+      <MovieOnderdeel v-for="(item, index) in films" :key="item.id" :defilm="item" :index="index"/>
+  </div>
+</template>
---
+++ src/components/JEvents.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+const header = ref('Yes')
+const dingen = ref([
+    {id: 1, label:'10 hoedjes'},
+    {id: 2, label:'5 hoedjes'},
+    {id: 3, label:'3 hoedjes'},
+])
+const newDing = ref("")
+const newDingUrgent = ref(false)
+</script>
+<template>
+    <h1>{{ header }}</h1>
+    <form class="add-item-form"
+        @submit.prevent="dingen.push({id: dingen.length + 1,label:newDing})"
+    >
+        <input 
+            v-model.trim="newDing" 
+            type="text" 
+            placeholder="voeg toe"
+        >
+        <label>
+            <input type="checkbox" v-model="newDingUrgent">
+        </label>
+        <button 
+             
+            class="btn btn-primary"
+        >Save</button>
+    </form>
+    <ul>
+    <li v-for="({id, label}, index) in dingen" :key="id">
+        {{ label }}
+    </li>
+    </ul>
+</template>
